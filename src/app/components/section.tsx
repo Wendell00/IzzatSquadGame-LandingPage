c12daac27239243b9e8e644fc87e35473a222cef
wendell00 <wendellborges2003@gmail.com>
2023-06-01 20:45:13 -0300

    main ajusts

--- src/app/components/section.tsx
+++ src/app/components/section.tsx
@@ -1,11 +1,40 @@
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 export default function Section() {
     return (
         <>
             <div className="h-screen w-screen max-w-[1920px] max-h-[1080px]"></div>
             <div className="w-screen h-screen main relative z-40">
+            {/* <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={3}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+                onSwiper={(swiper) => console.log(swiper)}
+                onSlideChange={() => console.log('slide change')}
+                >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+
+            </Swiper> */}
 
             </div>
         </>
     )
-  }
+  };
+
+
   
